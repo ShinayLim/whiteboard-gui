1dc4811f1e2261ef05f7a2c3a446163f31548590
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2023-08-03 15:16:25 +0800

    add import button

--- whiteboard.py
+++ whiteboard.py
@@ -25,9 +25,9 @@ eraser = PhotoImage(file=eraser_path)
 Button(root, image=eraser, bg="white", command=lambda: print("Eraser button clicked")).place(x=10, y=400)
 
 # import button
-import_path = os.path.join(current_dir, "eraser.png")
-eraser = PhotoImage(file=eraser_path)
-Button(root, image=eraser, bg="white", command=lambda: print("Eraser button clicked")).place(x=10, y=400)
+import_path = os.path.join(current_dir, "import.png")
+import_image = PhotoImage(file=import_path)
+Button(root, image=import_image, bg="white", command=lambda: print("Import button clicked")).place(x=10, y=400)
 
 # main screen
 canvas = Canvas(root, width=930, height=500, background="white", cursor="hand2")
